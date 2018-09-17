b475dbb54a3b7c73094e55452b6afcc123cada07
Wellington Lopes Cordeiro <wellington.lopes@neppoadm.com.br>
2018-09-17 13:20:30 -0300

    Listando o IMC de todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -2,33 +2,36 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutriocionista"
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i]
+  var tdPeso = paciente.querySelector(".info-peso");
+  var tdAltura = paciente.querySelector(".info-altura");
+  var tdImc = paciente.querySelector(".info-imc");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
 
-var alturaEhValida = true;
-var pesoEhValido = true;
+  var alturaEhValida = true;
+  var pesoEhValido = true;
 
-if(peso <= 0 || peso > 350) {
-  console.log("Peso inválido");
-  tdImc.textContent = "Peso inválido!";
-  pesoEhValido = false;
-}
+  if(peso <= 0 || peso > 350) {
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+  }
 
-if(altura <= 0 || altura >= 2.5) {
-  console.log("Altura inválida");
-  tdImc.textContent = "Altura inválida!"
-  alturaEhValida = false;
-}
+  if(altura <= 0 || altura >= 2.5) {
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida!"
+    alturaEhValida = false;
+  }
 
-if(pesoEhValido && alturaEhValida) {
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
-} else {
-  tdImc.textContent = "Altura e/ou peso inválidos!";
+  if(pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  } else {
+    tdImc.textContent = "Altura e/ou peso inválidos!";
+  }
 }
