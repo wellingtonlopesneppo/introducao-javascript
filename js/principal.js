2a842f14d681be087029a23f6aa92dc8382a828a
Wellington Lopes Cordeiro <wellington.lopes@neppoadm.com.br>
2018-09-17 11:38:42 -0300

    Buscando os dados do primeiro paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,15 @@
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutriocionista"
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
