2818122330812c9bd50fe48289e9cf51f1217336
Wellington Lopes Cordeiro <wellington.lopes@neppoadm.com.br>
2018-09-17 11:45:04 -0300

    Validando os dados do usuario

--- js/principal.js
+++ js/principal.js
@@ -6,10 +6,29 @@ var paciente = document.querySelector("#primeiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
 var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
 
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if(peso <= 0 || peso > 350) {
+  console.log("Peso inválido");
+  tdImc.textContent = "Peso inválido!";
+  pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 2.5) {
+  console.log("Altura inválida");
+  tdImc.textContent = "Altura inválida!"
+  alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+} else {
+  tdImc.textContent = "Altura e/ou peso inválidos!";
+}
