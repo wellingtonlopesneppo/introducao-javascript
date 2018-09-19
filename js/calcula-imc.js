8fde6b42a43b469dd1bbea87c5552f8c50532408
Wellington Lopes Cordeiro <wellington.lopes@neppoadm.com.br>
2018-09-19 14:59:12 -0300

    validando o form

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -6,29 +6,30 @@ var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
   var paciente = pacientes[i];
-  var tdPeso = paciente.querySelector(".info-peso");
-  var tdAltura = paciente.querySelector(".info-altura");
-  var tdImc = paciente.querySelector(".info-imc");
 
+  var tdPeso = paciente.querySelector(".info-peso");
   var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
   var altura = tdAltura.textContent;
 
-  var alturaEhValida = true;
-  var pesoEhValido = true;
+  var tdImc = paciente.querySelector(".info-imc");
 
-  if(peso <= 0 || peso >= 350) {
+
+  var alturaEhValida = validaAltura(altura);
+  var pesoEhValido = validaPeso(peso);
+
+  if(!pesoEhValido) {
     console.log("Peso inválido");
     tdImc.textContent = "Peso inválido!";
     pesoEhValido = false;
-
     paciente.classList.add("paciente-invalido");
   }
 
-  if(altura <= 0 || altura >= 2.5) {
+  if(!alturaEhValida) {
     console.log("Altura inválida");
     tdImc.textContent = "Altura inválida!"
     alturaEhValida = false;
-
     paciente.classList.add("paciente-invalido");
   }
 
@@ -42,4 +43,20 @@ function calculaImc(peso, altura) {
   var imc = 0;
   imc = peso / (altura * altura);
   return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+  if (peso > 0 && peso <= 350) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if (altura > 0 && altura < 2.5) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- js/form.js
+++ js/form.js
@@ -3,16 +3,25 @@ botaoAdicionar.addEventListener("click", function(event) {
   event.preventDefault();
 
   var form = document.querySelector("#form-adiciona");
-
   var paciente = obtemPacienteDoFormulario(form);
-
   var pacienteTr = montaTr(paciente);
 
+  var erros = validaPaciente(paciente);
+
+  if (erros.length > 0) {
+    exibeMensagensDeErro(erros);
+
+    return;
+
+  }
+
   var tabela = document.querySelector("#tabela-pacientes");
-
   tabela.appendChild(pacienteTr);
-
   form.reset();
+
+  var mensagensErro = document.querySelector("#tmensagens-erro");
+  mensagensErro.innerHTML = "";
+
 });
 
 function obtemPacienteDoFormulario(form) {
@@ -26,14 +35,6 @@ function obtemPacienteDoFormulario(form) {
   return paciente;
 }
 
-function montaTd(dado, classe) {
-  var td = document.createElement("td");
-  td.classList.add(classe);
-  td.textContent = dado;
-
-  return td;
-}
-
 function montaTr(paciente) {
   var pacienteTr = document.createElement("tr");
   pacienteTr.classList.add("paciente");
@@ -45,4 +46,49 @@ function montaTr(paciente) {
   pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
   return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.classList.add(classe);
+  td.textContent = dado;
+
+  return td;
+}
+
+function validaPaciente(paciente) {
+  var erros = [];
+
+  if (paciente.nome.length == 0) {
+    erros.push("O nome não pode ser em branco");
+  }
+
+  if (paciente.peso.length == 0) {
+    erros.push("O peso não pode ser em branco");
+  }
+
+  if (paciente.altura.length == 0) {
+    erros.push("A altura não pode ser em branco");
+  }
+
+  if (!validaPeso(paciente.peso)) {
+    erros.push("Peso é inválido");
+  }
+
+  if (!validaAltura(paciente.altura)) {
+    erros.push("Altura é invalida");
+  }
+
+  return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+  erros.forEach(function(erro) {
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
 }
